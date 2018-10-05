a60f3f437efd11864280bcb5d192e1731fbefc02
williammunnich <williammunnich@gmail.com>
2018-10-05 13:12:13 -0500

    add letters

--- src/com/swcompsci/randname.java
+++ src/com/swcompsci/randname.java
@@ -7,82 +7,82 @@ public class randname {
            return "A";
         }
         else if(number1 == 2) {
-
+           return "A";
         }
        else if(number1 == 3) {
-
+           return "A";
        }
        else if(number1 == 4) {
-
+           return "A";
        }
        else if(number1 == 5) {
-
+           return "A";
        }
        else if(number1 == 6) {
-
+           return "A";
        }
        else if(number1 == 7) {
-
+           return "A";
        }
        else if(number1 == 8) {
-
+           return "A";
        }
        else if(number1 == 9) {
-
+           return "A";
        }
        else if(number1 == 10) {
-
+           return "A";
        }
        else if(number1 == 11) {
-
+           return "A";
        }
        else if(number1 == 12) {
-
+           return "A";
        }
        else if(number1 == 13) {
-
+           return "A";
        }
        else if(number1 == 14) {
-
+           return "A";
        }
        else if(number1 == 15) {
-
+           return "A";
        }
        else if(number1 == 16) {
-
+           return "A";
        }
        else if(number1 == 17) {
-
+           return "A";
        }
        else if(number1 == 18) {
-
+           return "A";
        }
        else if(number1 == 19) {
-
+           return "A";
        }
        else if(number1 == 20) {
-
+           return "A";
        }
        else if(number1 == 21) {
-
+           return "A";
        }
        else if(number1 == 22) {
-
+           return "A";
        }
        else if(number1 == 23) {
-
+           return "A";
        }
        else if(number1 == 24) {
-
+           return "A";
        }
        else if(number1 == 25) {
-
+           return "A";
        }
        else if(number1 == 26) {
-
+           return "A";
        }
        else {
-             return "a"
+             return "a";
        }
        }
 
